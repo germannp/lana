6ee06fc27840e9a3b9e7b73f7a8c357cb6bd7d4a
Philipp Germann <philipp.germann@crg.eu>
2014-09-23 20:25:19 +0200

    Replaced motility_plot to take classes.

--- lana.py
+++ lana.py
@@ -33,94 +33,7 @@ def silly_walk(positions=None, steps=1600, ncells=30, ndim=2):
     return positions
 
 
-def motility_plot(velocities, turning_angles, displacements, times='',
-        legends='', ndim=2, save_as='', palette='deep'):
-    """Plots aspects of motility for single arrays or lists of arrays"""
-    if isinstance(velocities, list):
-        nmea_velo = float(velocities.__len__())
-    else:
-        nmea_velo = 1
-        foo = []
-        foo.append(velocities)
-        velocities = foo
-
-    if isinstance(turning_angles, list):
-        nmea_angles = float(turning_angles.__len__())
-    else:
-        nmea_angles = 1
-        foo = []
-        foo.append(turning_angles)
-        turning_angles = foo
-
-    if isinstance(displacements, list):
-        nmea_displ = float(displacements.__len__())
-    else:
-        nmea_displ = 1
-        foo = []
-        foo.append(displacements)
-        displacements = foo
-
-    if not times == '' and times.__len__() != nmea_displ:
-        foo = []
-        foo.append(times)
-        times = foo
-
-    sns.set(style="white", palette=sns.color_palette(
-        palette, int(max(nmea_velo, nmea_displ, nmea_angles))))
-    sns.set_context("paper", font_scale=1.5)
-    figure, axes = plt.subplots(ncols=3, figsize=(12,6))
-    plt.tight_layout()
-    plt.subplots_adjust(top=0.9)
-    plt.setp(axes, yticks=[])
-    plt.setp(axes, xticks=[])
-
-    axes[0].set_title('Velocities')
-    axes[0].set_xlim(0, np.max([np.percentile(v, 99.5) for v in velocities]))
-    axes[0].set_xticks([0])
-    axes[0].set_xticklabels([r'$0$'])
-    for velocity in velocities:
-        sns.kdeplot(velocity, shade=True, ax=axes[0])
-
-    axes[1].set_title('Turning Angles')
-    axes[1].set_xlim([0,np.pi])
-    axes[1].set_xticks([0, np.pi/2, np.pi])
-    axes[1].set_xticklabels([r'$0$', r'$\pi/2$', r'$\pi$'])
-    for turning_angle in turning_angles:
-        if ndim == 2: # Mirror data to get flat boundaries
-            turning_angle = np.concatenate((
-                -turning_angle, turning_angle, 2*np.pi-turning_angle))
-        sns.kdeplot(turning_angle, shade=True, ax=axes[1])
-    if ndim == 2:
-        axes[1].plot([0, np.pi], [1/(3*np.pi), 1/(3*np.pi)], '--k')
-    if ndim == 3:
-        x = np.arange(0, np.pi, 0.1)
-        axes[1].plot(x, np.sin(x)/2, '--k')
-
-    axes[2].set_title('Mean Displacements')
-    axes[2].set_xlabel('sqrt of time')
-    # axes[2].set_xlim(0,100)
-    for i, displacement in enumerate(displacements):
-        if times == '':
-            time = range(0, displacement.shape[-2])
-        else:
-            time = times[i]
-        color = sns.color_palette(n_colors=i+1)[-1]
-        if legends == '':
-            legend = False
-            label = ''
-        else:
-            legend = True
-            label = legends[i]
-        sns.tsplot(displacement.T, time=np.sqrt(time), ax=axes[2], color=color,
-            legend=legend, condition=label)
-
-    if save_as == '':
-        plt.show()
-    else:
-        plt.savefig('{}.png'.format(save_as))
-
-
-def motility_classes_plot(motilities, save_as='', palette='deep'):
+def motility_plot(motilities, save_as='', palette='deep'):
     """Plots aspects of motility for a (list of) Motility class(es)"""
     if not isinstance(motilities, list):
         foo = []
@@ -220,7 +133,7 @@ class Motility:
         return self.positions[-1]**2/(4*self.positions.shape[0]*self.timestep)
 
     def plot(self, *args, **kwargs):
-        motility_classes_plot(self, *args, **kwargs)
+        motility_plot(self, *args, **kwargs)
 
 
 if __name__ == "__main__":
