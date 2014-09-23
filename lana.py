d2f1cf31ed96620bd4f319c33576b1155691ee3d
Philipp Germann <philipp.germann@crg.eu>
2014-09-23 20:05:28 +0200

    Added function to plot Motility classes.

--- lana.py
+++ lana.py
@@ -1,4 +1,4 @@
-"""Tools to analyze  cell motility from positions within lymph nodes"""
+"""Tools to analyze and plot cell motility from positions within lymph nodes"""
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -34,8 +34,7 @@ def silly_walk(positions=None, steps=1600, ncells=30, ndim=2):
 
 
 def motility_plot(velocities, turning_angles, displacements, times='',
-        legends='', rand_angles_2D=True, rand_angles_3D=False,
-        save_as='', palette='deep'):
+        legends='', ndim=2, save_as='', palette='deep'):
     """Plots aspects of motility for single arrays or lists of arrays"""
     if isinstance(velocities, list):
         nmea_velo = float(velocities.__len__())
@@ -87,18 +86,19 @@ def motility_plot(velocities, turning_angles, displacements, times='',
     axes[1].set_xticks([0, np.pi/2, np.pi])
     axes[1].set_xticklabels([r'$0$', r'$\pi/2$', r'$\pi$'])
     for turning_angle in turning_angles:
-        turning_angle = np.concatenate((
-            -turning_angle, turning_angle, 2*np.pi-turning_angle))
+        if ndim == 2: # Mirror data to get flat boundaries
+            turning_angle = np.concatenate((
+                -turning_angle, turning_angle, 2*np.pi-turning_angle))
         sns.kdeplot(turning_angle, shade=True, ax=axes[1])
-    if rand_angles_2D:
+    if ndim == 2:
         axes[1].plot([0, np.pi], [1/(3*np.pi), 1/(3*np.pi)], '--k')
-    if rand_angles_3D:
+    if ndim == 3:
         x = np.arange(0, np.pi, 0.1)
-        axes[1].plot(x, np.sin(x)/6, '--k')
+        axes[1].plot(x, np.sin(x)/2, '--k')
 
     axes[2].set_title('Mean Displacements')
     axes[2].set_xlabel('sqrt of time')
-    axes[2].set_xlim(0,100)
+    # axes[2].set_xlim(0,100)
     for i, displacement in enumerate(displacements):
         if times == '':
             time = range(0, displacement.shape[-2])
@@ -120,9 +120,65 @@ def motility_plot(velocities, turning_angles, displacements, times='',
         plt.savefig('{}.png'.format(save_as))
 
 
+def motility_classes_plot(motilities, save_as='', palette='deep'):
+    """Plots aspects of motility for a (list of) Motility class(es)"""
+    if not isinstance(motilities, list):
+        foo = []
+        foo.append(motilities)
+        motilities = foo
+
+    sns.set(style="white", palette=sns.color_palette(
+        palette, int(max(10,1))))
+    sns.set_context("paper", font_scale=1.5)
+    figure, axes = plt.subplots(ncols=3, figsize=(12,6))
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.9)
+    plt.setp(axes, yticks=[])
+    plt.setp(axes, xticks=[])
+
+    axes[0].set_title('Velocities')
+    axes[0].set_xlim(0, np.max([np.percentile(v, 99.5) for v in 
+        [motility.velocities() for motility in motilities]]))
+    axes[0].set_xticks([0])
+    axes[0].set_xticklabels([r'$0$'])
+
+    axes[1].set_title('Turning Angles')
+    axes[1].set_xlim([0,np.pi])
+    axes[1].set_xticks([0, np.pi/2, np.pi])
+    axes[1].set_xticklabels([r'$0$', r'$\pi/2$', r'$\pi$'])
+    # if motility.ndim == 2:
+    #     axes[1].plot([0, np.pi], [1/(3*np.pi), 1/(3*np.pi)], '--k')
+    # if motility.ndim == 3:
+    #     x = np.arange(0, np.pi, 0.1)
+    #     axes[1].plot(x, np.sin(x)/2, '--k')
+
+    axes[2].set_title('Mean Displacements')
+    axes[2].set_xlabel('sqrt of time')
+
+    for i, motility in enumerate(motilities):
+        sns.kdeplot(motility.velocities().reshape(-1), shade=True, ax=axes[0])
+        turning_angles = motility.turning_angles().reshape(-1)
+        if motility.ndim == 2:
+            turning_angles = np.concatenate((
+                -turning_angles, turning_angles, 2*np.pi-turning_angles))
+        sns.kdeplot(turning_angles, shade=True, ax=axes[1])
+        if motility.label == '':
+            legend = False
+        else:
+            legend = True
+        color = sns.color_palette(n_colors=i+1)[-1]
+        sns.tsplot(motility.displacements().T, time=np.sqrt(motility.times()),
+            legend=legend, condition=motility.label, ax=axes[2], color=color)
+
+    if save_as == '':
+        plt.show()
+    else:
+        plt.savefig('{}.png'.format(save_as))
+
+
 class Motility:
     """Analyzes motility of attributed numpy array of positions"""
-    def __init__(self, positions=None, ndim=2, timestep=25):
+    def __init__(self, positions=None, ndim=2, timestep=25, label=''):
         if positions == None:
             self.positions = silly_walk(ndim=ndim)
         else:
@@ -130,6 +186,7 @@ class Motility:
         self.ndim = ndim
         self.ncells = self.positions.shape[1]/ndim
         self.timestep = int(timestep)
+        self.label = label
         print('Analyzing {} cells, every {} seconds'.format(
             self.ncells, self.timestep))
 
@@ -163,9 +220,7 @@ class Motility:
         return self.positions[-1]**2/(4*self.positions.shape[0]*self.timestep)
 
     def plot(self, *args, **kwargs):
-        motility_plot(
-            self.velocities().reshape(-1), self.turning_angles().reshape(-1),
-            self.displacements(), self.times(), *args, **kwargs)
+        motility_classes_plot(self, *args, **kwargs)
 
 
 if __name__ == "__main__":
