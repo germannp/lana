217bcbd45918b7fbfa30c889f2becff03dca239b
Philipp Germann <philipp.germann@crg.eu>
2014-10-01 14:48:43 +0200

    Use 'Track ID' instead of 'timepoint' to identify tracks.

--- volocity.py
+++ volocity.py
@@ -12,8 +12,8 @@ def read_tracks(volocity_file, min_track_length=5):
 		if 'Centroid X' in line:
 			words = line.split('\t')
 			for j, word in enumerate(words):
-				if 'Timepoint' in word:
-					index_time = j
+				if 'Track ID' in word:
+					index_track_id = j
 				if 'Centroid X' in word:
 					data_begin = i + 1
 					index_X = j
@@ -22,23 +22,19 @@ def read_tracks(volocity_file, min_track_length=5):
 	for i, line in enumerate(lines[data_begin:]):
 		words = line.split('\t')
 
-		try:
-			new_time = int(words[index_time])
-			if track == []:
-				old_time = new_time - 1
-		except ValueError:
-			new_time = -666
+		if track == []:
+			track_id = words[index_track_id]
 
-		if new_time == old_time + 1:
-			track.append(map(float, words[index_X:index_X+3]))
-			old_time = new_time
+		if words[index_track_id] == track_id:
+			if words[index_X] != 'N/A':
+				track.append(map(float, words[index_X:index_X+3]))
 		else:
+			track_id = words[index_track_id]
 			if track.__len__() >= min_track_length:
 				tracks.append(np.array(track))
 			track = []
-			if new_time != -666: # For new tracks without N/A!
+			if words[index_X] != 'N/A':
 				track.append(map(float, words[index_X:index_X+3]))
-				old_time = new_time
 	else:
 		if track.__len__() >= min_track_length:
 			tracks.append(np.array(track))
@@ -51,3 +47,4 @@ if __name__ == '__main__':
 	volocity_example = lana.Motility(tracks, ndim=3, timestep=1)
 	volocity_example.plot()
 	lana.plot_tracks(volocity_example)
+	print([track.__len__() for track in volocity_example.tracks])
