6da8cb92bff763bc7f2435d12e5ccfa94b30a5dc
Philipp Germann <philipp.germann@crg.eu>
2015-01-16 19:35:31 +0100

    Use pandas

--- lana.py
+++ lana.py
@@ -1,6 +1,4 @@
 """Tools to analyze and plot cell motility from tracks within lymph nodes"""
-import random
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -35,37 +33,52 @@ def silly_track(init_position=None, steps=25, step_size=1):
     return track
 
 
-def plot_tracks(motilities, ntracks=15, save_as='', palette='deep'):
-    """Plots the x-y-tracks of a (list of) Motility class(es)"""
-    if not isinstance(motilities, list):
-        foo = []
-        foo.append(motilities)
-        motilities = foo
+def silly_tracks(ntracks=25):
+    """Generates a DataFrame with tracks"""
+    tracks = pd.DataFrame()
+    for track_id in range(ntracks):
+        track = silly_track()
+        tracks = tracks.append(pd.DataFrame({
+            'Track_ID': track_id, 'X': track[:,0], 'Y': track[:,1]}))
+    return tracks
 
-    ndim = min([motility.ndim for motility in motilities])
-    ntracks = min(ntracks, min([motility.tracks.__len__() for motility in motilities]))
+
+def plot_tracks(tracks, save_as='', palette='deep'):
+    """Plots the x-y-tracks of a (list of) Motility class(es)"""
+    if 'Z' in tracks.columns:
+        ndim = 3
+    else:
+        ndim = 2
+
+    if 'Condition' not in tracks.columns:
+        tracks['Condition'] = 'Default'
 
     sns.set(style="white", palette=sns.color_palette(
-        palette, motilities.__len__()))
+        palette, tracks['Condition'].unique().__len__()))
     sns.set_context("paper", font_scale=1.5)
 
     plt.title('Superimposed Tracks')
-    for i in range(ndim-1):
+    for i, dim in enumerate(('Y', 'Z')[:ndim-1]):
         plt.subplot(1, ndim-1, i+1)
         plt.axis('equal')
-        for j, motility in enumerate(motilities):
+        plt.ylabel('x-axis')
+        plt.xlabel(['y-axis', 'z-axis'][i])
+        for j, (cond, cond_tracks) in enumerate(tracks.groupby('Condition')):
             color = sns.color_palette()[j]
-            for track in random.sample(motility.tracks, ntracks):
-                plt.ylabel('x-axis')
-                plt.xlabel(['y-axis', 'z-axis'][i])
-                plt.plot(track[:,0]-track[0,0], track[:,i+1]-track[0,i+1],
-                    color=color)
+            for (track_nr, track) in cond_tracks.groupby('Track_ID'):
+                if track_nr == 0:
+                    label = cond
+                else:
+                    label = ''
+                plt.plot(track['X']-track['X'][0], track[dim]-track[dim][0],
+                    color=color, label=label)
                 # track = track - track[1]
                 # track = track.reshape(-1, 1, 2)
                 # segments = np.concatenate([track[:-1], track[1:]], axis=1)
                 # lc = LineCollection(segments, cmap=plt.get_cmap('copper'))
                 # plt.gca().add_collection(lc)
 
+    plt.legend()
     plt.tight_layout()
     if save_as == '':
         plt.show()
@@ -73,56 +86,84 @@ def plot_tracks(motilities, ntracks=15, save_as='', palette='deep'):
         plt.savefig('{}.png'.format(save_as))
 
 
-def animate_tracks(motilities, ntracks=15, palette='deep'):
+def animate_tracks(tracks, palette='deep'):
     """Shows an animation of the tracks"""
-    if not isinstance(motilities, list):
-        foo = []
-        foo.append(motilities)
-        motilities = foo
+    if 'Condition' not in tracks.columns:
+        tracks['Condition'] = 'Default'
 
-    ndim = min([motility.ndim for motility in motilities])
-    ntracks = min(ntracks, min([motility.tracks.__len__() 
-        for motility in motilities]))
-    tmax = max([track.shape[0] 
-        for track in motility.tracks 
-        for motility in motilities])
+    if 'Time' not in tracks.columns:
+        tracks['Time'] = tracks.index
 
     sns.set(style="white", palette=sns.color_palette(
-        palette, motilities.__len__()))
+        palette, tracks['Condition'].unique().__len__()))
     sns.set_context("paper", font_scale=1.5)
 
     plt.title('Animated Tracks')
-    plt.axis('equal')
-    if ndim == 3:
+    # TODO: Scale axis properly ...
+    if 'Z' in tracks.columns:
         ax = plt.gca(projection='3d')
-    sample = random.sample(motility.tracks, ntracks)
-    for t in range(tmax):
-        for j, motility in enumerate(motilities):
-            positions = np.empty((0, ndim))
-            for track in sample:
-                try:
-                    positions = np.append(positions, track[t])
-                except:
-                    pass
-            positions = positions.reshape(-1,ndim)
-            color = sns.color_palette()[j]
+    else:
+        plt.axis('equal')
+
+    for t, posis in tracks.groupby('Time'):
+        for j, (cond, cond_posis) in enumerate(posis.groupby('Condition')):
             plt.clf()
-            if ndim == 3:
-                ax.scatter(positions[:,0], positions[:,1], positions[:,2], c='red')
+            if 'Z' in cond_posis.columns:
+                ax.scatter(cond_posis['X'], cond_posis['Y'], cond_posis['Z'], c='red')
             else:
-                points = plt.plot(positions[:,0], positions[:,1], 'o')
+                plt.plot(posis['X'], posis['Y'], 'o')
         plt.pause(1)
 
 
-def plot_motility(motilities, save_as='', palette='deep'):
+def analyze_track(track):
+    """Calculates displacements, velocities and turning angles for a track"""
+    if 'Z' in track.columns:
+        positions = track[['X', 'Y', 'Z']]
+    else:
+        positions = track[['X', 'Y']]
+
+    track['Displacement'] = np.linalg.norm(positions - positions.iloc[0], axis=1)
+
+    dr = positions.diff()
+    dr_norms = np.linalg.norm(dr, axis = 1)
+
+    track['Velocity'] = dr_norms/track['Time'].diff()
+
+    dot_products = np.sum(dr.shift(-1)*dr, axis=1)
+    norm_products = dr_norms[1:]*dr_norms[:-1]
+
+    track['Turning Angle'] = np.arccos(dot_products[:-1]/norm_products)
+
+    return track
+
+
+def analyze_tracks(tracks):
+    """Prepares tracks for analysis"""
+    if 'Condition' not in tracks.columns:
+        tracks['Condition'] = 'Default'
+
+    if 'Time' not in tracks.columns:
+        print('Warning: no time given, using index!')
+        tracks['Time'] = tracks.index
+
+    if sum(tracks[['Condition', 'Track_ID', 'Time']].duplicated()) != 0:
+        print('Error: Tracks not unique, aborting analysis.')
+        return
+
+    return tracks.groupby(['Condition', 'Track_ID']).apply(analyze_track)
+
+
+def plot_motility(tracks, save=False, palette='deep'):
     """Plots aspects of motility for a (list of) Motility class(es)"""
-    if not isinstance(motilities, list):
-        foo = []
-        foo.append(motilities)
-        motilities = foo
+    if not set(['Velocity', 'Turning Angle']).issubset(tracks.columns):
+        print('Error: data not found, tracks must be analyzed first.')
+        return
+
+    if 'Condition' not in tracks.columns:
+        tracks['Condition'] = 'Default'
 
     sns.set(style="white", palette=sns.color_palette(
-        palette, motilities.__len__()))
+        palette, tracks['Condition'].unique().__len__()))
     sns.set_context("paper", font_scale=1.5)
     figure, axes = plt.subplots(ncols=3, figsize=(12,6))
     plt.tight_layout()
@@ -131,8 +172,7 @@ def plot_motility(motilities, save_as='', palette='deep'):
     plt.setp(axes, xticks=[])
 
     axes[0].set_title('Velocities')
-    axes[0].set_xlim(0, np.max([np.percentile(v, 99.5) for v in 
-        [motility.velocities() for motility in motilities]]))
+    axes[0].set_xlim(0, np.percentile(tracks['Velocity'].dropna(), 99.5))
     axes[0].set_xticks([0])
     axes[0].set_xticklabels([r'$0$'])
 
@@ -142,110 +182,57 @@ def plot_motility(motilities, save_as='', palette='deep'):
     axes[1].set_xticklabels([r'$0$', r'$\pi/2$', r'$\pi$'])
 
     axes[2].set_title('Mean Displacements')
-    axes[2].set_xlabel('sqrt of time')
+    axes[2].set_xlabel('Time')
 
-    for i, motility in enumerate(motilities):
-        if motility.label == '':
-            sns.kdeplot(motility.velocities(),
-                shade=True, ax=axes[0], gridsize=500)
+    for i, (cond, cond_tracks) in enumerate(tracks.groupby('Condition')):
+        sns.kdeplot(cond_tracks['Velocity'].dropna(), 
+            shade=True, ax=axes[0], gridsize=500, label=cond)
+        turning_angles = cond_tracks['Turning Angle'].dropna().as_matrix()
+        if 'Z' in tracks.index:
+            x = np.arange(0, np.pi, 0.1)
+            axes[1].plot(x, np.sin(x)/2, '--k')
         else:
-            sns.kdeplot(motility.velocities(), 
-                shade=True, ax=axes[0], gridsize=500, label=motility.label)
-        turning_angles = motility.turning_angles()
-        if motility.ndim == 2:
             turning_angles = np.concatenate(( # Mirror at boundaries.
                 -turning_angles, turning_angles, 2*np.pi-turning_angles))
             axes[1].plot([0, np.pi], [1/(3*np.pi), 1/(3*np.pi)], '--k')
-        if motility.ndim == 3:
-            x = np.arange(0, np.pi, 0.1)
-            axes[1].plot(x, np.sin(x)/2, '--k')
         sns.kdeplot(turning_angles, shade=True, ax=axes[1])
         color = sns.color_palette(n_colors=i+1)[-1]
-        sns.tsplot(motility.displacements(), time='sqrt of time', unit='cell',
-            value='displacement', ax=axes[2], color=color)
+        cond_tracks['Time'] = np.sqrt(cond_tracks['Time'])
+        sns.tsplot(cond_tracks, time='Time', unit='Track_ID',
+            value='Displacement', ax=axes[2], color=color)
 
-    if save_as == '':
-        plt.show()
+    if save:
+        conditions = [cond for cond in tracks['Condition'].unique()]
+        plt.savefig('Motility_' + '-'.join(conditions) + '.png')
     else:
-        plt.savefig('{}.png'.format(save_as))
+        plt.show()
 
 
-class Motility:
-    """Analyzes motility of attributed list of tracks"""
-    def __init__(self, tracks=None, ndim=2, timestep=20, label=''):
-        if tracks == None:
-            print('Simulating data')
-            rand = np.random.rand()
-            self.tracks = [silly_track(step_size=rand) for _ in range(100)]
+def plot_joint_motility(tracks, save=False, palette='deep'):
+    """Plots the joint distribution of the velocities and turning angles."""
+    if not set(['Velocity', 'Turning Angle']).issubset(tracks.columns):
+        print('Error: data not found, tracks must be analyzed first.')
+        return
+
+    sns.set(style="white", palette=sns.color_palette(
+        palette, tracks['Condition'].unique().__len__()))
+
+    for i, (cond, cond_tracks) in enumerate(tracks.groupby('Condition')):
+        color = sns.color_palette()[i]
+        sns.jointplot(tracks['Turning Angle'], tracks['Velocity'], kind='kde',
+            stat_func=None, xlim=[0, np.pi], space=0,
+            ylim=[0, np.percentile(tracks['Velocity'].dropna(), 99.5)])
+        if save:
+            plt.savefig('Joint-Motility_' + cond + '.png')
         else:
-            self.tracks = tracks
-        self.ndim = ndim # TODO: Automatize!
-        self.ncells = self.tracks.__len__()
-        self.timestep = int(timestep)
-        self.label = label
-        print('Analyzing {} cells, every {} seconds'.format(
-            self.ncells, self.timestep))
-
-    def velocities(self):
-        velocities = []
-        for track in self.tracks:
-            differences = (track[1:] - track[:-1])/self.timestep
-            velocities.append(np.linalg.norm(differences, axis=1))
-        return np.hstack(velocities)
-
-    def mean_velocities(self):
-        mean_velocities = []
-        for track in self.tracks:
-            differences = (track[1:] - track[:-1])/self.timestep
-            mean_velocities.append(np.mean(np.linalg.norm(
-                differences, axis=1)))
-        return np.hstack(mean_velocities)
-
-    def turning_angles(self):
-        turning_angles = []
-        for track in self.tracks:
-            differences = track[1:] - track[:-1]
-            dot_products = np.sum(differences[1:]*differences[:-1], axis=1)
-            difference_norms = np.linalg.norm(differences, axis=1)
-            norm_products = difference_norms[1:]*difference_norms[:-1]
-            turning_angles.append(np.arccos(
-                np.nan_to_num(np.clip(dot_products/norm_products, -1, 1))))
-        return np.hstack(turning_angles)
-
-    def displacements(self):
-        displacements = pd.DataFrame()
-        for i, track in enumerate(self.tracks):
-            time = np.arange(0, track.shape[0]*self.timestep, 
-                self.timestep).reshape(-1,1)
-            cell = np.ones((track.shape[0], 1))*i
-            displacement = np.linalg.norm(track - track[0], 
-                axis=1).reshape(-1,1)
-            displacements = displacements.append(pd.DataFrame(
-                np.hstack((time, np.sqrt(time), cell, displacement)), 
-                columns=['time', 'sqrt of time', 'cell', 'displacement']))
-        return displacements
-
-    # def motility_coeffs(self):
-    #     return self.tracks[-1]**2/(4*self.tracks.shape[0]*self.timestep)
-
-    def plot(self, *args, **kwargs):
-        plot_motility(self, *args, **kwargs)
-
-    def animate(self, *args, **kwargs):
-        animate_tracks(self, *args, **kwargs)
+            plt.show()
 
 
 if __name__ == "__main__":
     """Demostrates motility analysis of simulated data."""
-    # T_cells = Motility()
-    # T_cells.plot()
-    # plot_tracks(T_cells)
-
-    import os
-    import excalib2
-
-    os.chdir('../cpm_lymph_node_T-cell_persistence')
-    with excalib2.Simulation('persistence') as persistence:
-        T_cells = Motility(persistence.read_tracks(ndim=3), ndim=3)
-        plot_tracks(T_cells)
-        animate_tracks(T_cells)
+    tracks = silly_tracks()
+    plot_tracks(tracks)
+    # animate_tracks(tracks)
+    tracks = analyze_tracks(tracks)
+    plot_joint_motility(tracks)
+    plot_motility(tracks)
