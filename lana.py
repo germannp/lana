2b7b96aad500acc9709e012749c313eda98d27ef
Philipp Germann <philipp.germann@crg.eu>
2015-02-12 12:48:40 +0100

    Equalized axis in 3D plot of tracks.

--- lana.py
+++ lana.py
@@ -97,10 +97,41 @@ def plot_tracks(tracks, save=False, palette='deep'):
         plt.show()
 
 
+def equalize_axis3d(source_ax, zoom=1, target_ax=None):
+    '''after http://stackoverflow.com/questions/8130823/
+    set-matplotlib-3d-plot-aspect-ratio'''
+    if target_ax == None:
+        target_ax = source_ax
+    elif zoom != 1:
+        print('Zoom ignored when target axis is provided.')
+        zoom = 1
+    source_extents = np.array([getattr(source_ax, 'get_{}lim'.format(dim))()
+        for dim in 'xyz'])
+    target_extents = np.array([getattr(target_ax, 'get_{}lim'.format(dim))()
+        for dim in 'xyz'])
+    spread = target_extents[:,1] - target_extents[:,0]
+    max_spread = max(abs(spread))
+    r = max_spread/2
+    centers = np.mean(source_extents, axis=1)
+    for center, dim in zip(centers, 'xyz'):
+        getattr(source_ax, 'set_{}lim'.format(dim))(center - r/zoom, center + r/zoom)
+
+
 def plot_tracks_3d(tracks):
-    fig = plt.figure()
+    sns.set_style('white')
+    fig = plt.figure(figsize=(8,8))
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot(tracks['X'], tracks['Y'], tracks['Z'])
+
+    criteria = [crit for crit in ['Condition', 'Sample', 'Track_ID']
+        if crit in tracks.dropna(axis=1).columns]
+    print(criteria)
+
+    for _, track in tracks.groupby(criteria):
+        ax.plot(track['X'].values, track['Y'].values, track['Z'].values)
+
+    equalize_axis3d(ax)
+    ax.set_aspect('equal')
+    plt.tight_layout()
     plt.show()
 
 
