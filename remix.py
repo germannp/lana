22a7a0964bc3a58456e24c695ed22c61e6670b2a
Philipp Germann <philipp.germann@crg.eu>
2015-02-13 09:46:13 +0100

    Fix track length issue ...

--- remix.py
+++ remix.py
@@ -15,7 +15,6 @@ def silly_3d_steps(track_data=None, n_steps=10):
         rolling_angles = (np.random.rand(n_steps-2) - 0.5)*2*np.pi
         condition = 'Random'
     else:
-        n_steps = track_data.__len__()
         velocities = track_data['Velocity'].dropna().values
         turning_angles = track_data['Turning Angle'].dropna().values
         rolling_angles = track_data['Rolling Angle'].dropna().values
@@ -23,6 +22,7 @@ def silly_3d_steps(track_data=None, n_steps=10):
             condition = track_data['Condition'].iloc[0] + ' Rebuilt'
         else:
             condition = 'Rebuilt'
+        n_steps = velocities.__len__()
 
     # Walk in x-y plane w/ given velocity and turning angles
     dr = np.zeros((n_steps+1, 3))
@@ -66,7 +66,7 @@ def remix(tracks=None, n_tracks=50, n_steps=60):
     for i in range(n_tracks):
         track_data = velo_and_turn.ix[np.random.choice(velo_and_turn.index.values, 1)]
         track_data = track_data.append(
-            remaining_data.ix[np.random.choice(remaining_data.index.values, n_steps-3)])
+            remaining_data.ix[np.random.choice(remaining_data.index.values, n_steps-2)])
         track_data = track_data.append(pd.DataFrame({'Velocity':
             velocities_only[np.random.choice(velocities_only.index.values, 1)]}))
         new_track = silly_3d_steps(track_data)
@@ -88,26 +88,26 @@ if __name__ == '__main__':
 
     """Rebuild a single track"""
     tracks = pd.read_csv('Examples/ctrl_data.csv')
-    # ctrl = tracks[tracks.Track_ID == 1015.0]
-    # ctrl[['X', 'Y', 'Z']] = ctrl[['X', 'Y', 'Z']] - ctrl[['X', 'Y', 'Z']].iloc[-1]
+    ctrl = tracks[tracks.Track_ID == 1015.0]
+    ctrl[['X', 'Y', 'Z']] = ctrl[['X', 'Y', 'Z']] - ctrl[['X', 'Y', 'Z']].iloc[-1]
     # rebuilt = silly_3d_steps(ctrl)
-    # lana.plot_tracks_3d(ctrl.append(rebuilt)) # TODO: Nice rotation ...
+    # # lana.plot_tracks_3d(ctrl.append(rebuilt)) # TODO: Nice rotation ...
     # rebuilt = lana.analyze_motility(rebuilt)
     # print(ctrl[['Time', 'Velocity', 'Turning Angle', 'Rolling Angle']])
     # print(rebuilt[['Time', 'Velocity', 'Turning Angle', 'Rolling Angle']])
 
 
     """Remix Ctrl"""
-    # remix = remix(ctrl, n_tracks=1, n_steps=6)
+    # remix = remix(ctrl, n_tracks=1, n_steps=5)
     # remix = lana.analyze_motility(remix)
     # print(remix[['Time', 'Velocity', 'Turning Angle', 'Rolling Angle']])
     # print(ctrl[['Time', 'Velocity', 'Turning Angle', 'Rolling Angle']])
 
 
     """Remix tracks"""
-    remix = remix(tracks)
-    tracks = tracks.append(remix).reset_index()
-    tracks = lana.analyze_motility(tracks)
-    lana.plot_motility(tracks)
-    lana.lag_plot(tracks)
-    lana.plot_joint_motility(tracks[tracks.Condition == 'Ctrl Remixed'])
+    # remix = remix(tracks)
+    # tracks = tracks.append(remix).reset_index()
+    # tracks = lana.analyze_motility(tracks)
+    # lana.plot_motility(tracks)
+    # lana.lag_plot(tracks)
+    # lana.plot_joint_motility(tracks[tracks.Condition == 'Ctrl Remixed'])
