3d1845ef47c59b2da3fe9883264c10bbee3483b5
Philipp Germann <philipp.germann@crg.eu>
2015-09-25 14:49:51 +0200

    label_axes() to add A, B ... to subplots

--- lana/utils.py
+++ lana/utils.py
@@ -1,5 +1,8 @@
 """Utilities"""
+from string import ascii_uppercase
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def track_identifiers(tracks):
@@ -28,3 +31,11 @@ def equalize_axis3d(source_ax, zoom=1, target_ax=None):
     for center, dim in zip(centers, 'xyz'):
         getattr(source_ax, 'set_{}lim'.format(dim))(center-r/zoom, center+r/zoom)
     source_ax.set_aspect('equal')
+
+
+def label_axes():
+    """Label panels; after http://stackoverflow.com/questions/25543978/"""
+    # TODO: not 3D ready, works only for axis w/o ticks
+    for i, ax in enumerate(plt.gcf().axes):
+        ax.text(-0.075, 0.96, ascii_uppercase[i], transform=ax.transAxes,
+            size=20, weight='bold')
